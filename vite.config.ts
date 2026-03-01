7d413733b15464bd0012e393a976106dc7bc3100
Mubin Manasia <fashionag615@gmail.com>
2026-03-01 14:01:06 +0530

    Removed GitHub Pages base config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
-      // Add this line below. Replace 'og-in-worldwide' if your repo name is different.
-      base: '/boom-lift-rentals/', 
       define: {
         'process.env.API_KEY' : JSON.stringify('api-key-this-is-not-used-can-be-ignored!'),
       },
